81b5ac475398f3040c2fe376a746171e75d643e1
Stagiaire05 <devsecpem@proton.me>
2026-03-19 11:04:38 +0100

    fix Supp credential

--- src/config.php
+++ src/config.php
@@ -5,7 +5,7 @@
 $db_host = 'localhost';
 $db_name = 'navstrike_db';
 $db_user = 'root';
-$db_pass = 'N@vStr1ke_3ch0_r00t_2026!';
+$db_pass = $db_pass = getenv('DB_PASSWORD');
 
 // Direct connection without error handling
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
